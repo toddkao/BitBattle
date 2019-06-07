23a3407977acf6affcb6a2d350e6c75165188b01
Todd Kao <todd@unoapp.com>
2019-06-07 14:20:55 -0400

    Randomly generate board

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -7,7 +7,23 @@ const Map: React.FC = () => {
   const width = 10;
   const height = 10;
   const game = new Game(width, height);
-  // game.importBoard('PGGGGGDGGDDGGDDP');
+
+  function getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  let boardCards: any = {
+    0: 'P',
+    1: 'G',
+    2: 'D'
+  }
+  let randomBoard = '';
+  for (let i = 0; i < width * height; i++) {
+    let r = getRandomInt(0, 2);
+    randomBoard += boardCards[r];
+  }
+  console.log(randomBoard.length);
+  game.importBoard(randomBoard);
 
   const mapStyles = {
     gridTemplateColumns: `repeat(${width}, 1fr)`,
