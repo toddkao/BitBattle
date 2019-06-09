163a8350fd48a33ebbabafc8c9bdb95e57f2bc1b
Todd Kao <todd.kakao@gmail.com>
2019-06-09 11:37:59 -0400

    Update Sentinel1/Watchman1 naming to be consistent

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -3,8 +3,8 @@ import { TileObjectType } from "../game/types/tile-object-type";
 import EntityObject from "../game/cards/entity-object";
 import AttackDogCard from "../game/cards/attack-dog-card";
 import GuardPupCard from "../game/cards/guard-pup-card";
-import Sentinel1Card from "../game/cards/sentinel-1.card";
-import Watchman1Card from "../game/cards/watchman-card";
+import Sentinel1Card from "../game/cards/sentinel1-card";
+import Watchman1Card from "../game/cards/watchman1-card";
 
 export default {
   ObjectToClass(o: any): string {
